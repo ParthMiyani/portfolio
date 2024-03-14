2398a407c583bd32dd8922f7a81239b55224e80e
ParthMiyani <parthmiyani42@gmail.com>
2024-03-13 23:34:01 -0700

    add style to contact and refactor code in css

--- src/components/views/Contact.jsx
+++ src/components/views/Contact.jsx
@@ -1,15 +1,82 @@
 import "../../styles/contact.css";
+import { useState } from "react";
+import resume from "../../parth_miyani.pdf";
 
 export default function Contact() {
+  const [time, setTime] = useState(
+    new Date().toLocaleTimeString("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+      timeZone: "MST",
+    })
+  );
+
+  setInterval(() => {
+    setTime(
+      new Date().toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true,
+        timeZone: "MST",
+      })
+    );
+  }, 1000);
+
   return (
     <div className="contact">
-      <h1>Contact me!</h1>
-      <p>
-        Have a question, a project idea, a job opportunity or just want to say
-        hi? I'd love to hear from you! Feel free to reach out, and I'll get back
-        to you as soon as possible. Let's chat and make things happen!
-      </p>
-      <div></div>
+      <div className="text">
+        <h1>Contact me!</h1>
+        <p>
+          Have a question, a project idea, a job opportunity or just want to say
+          hi?
+          <br />
+          I'd love to hear from you! Feel free to reach out.
+          <br />
+          Let's chat and make things happen!
+        </p>
+        <div className="email">
+          <p className="arrow">→</p>
+          <a
+            className="email-link"
+            href="mailto:pmiyani42@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            pmiyani42@gmail.com
+          </a>
+        </div>
+      </div>
+      <div className="contact-links">
+        <div>
+          <a className="link" target="_blank" rel="noreferrer" href={resume}>
+            Resume
+          </a>
+          <a
+            className="link"
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/parth-miyani/"
+          >
+            LinkedIn
+          </a>
+          <a
+            className="link"
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/ParthMiyani"
+          >
+            GitHub
+          </a>
+          {/* <a className="link" href="https://twitter.com/parth_miyani_">→Twitter</a> */}
+        </div>
+        <div>
+          <p>
+            <span className="time">{time && time}</span>© Parth Miyani{" "}
+            {new Date().getFullYear()}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/views/Experiences.jsx
+++ src/components/views/Experiences.jsx
@@ -1,62 +1,24 @@
 import "../../styles/experiences.css";
+import experiences from "../../data/experiences.json";
 
 export default function Experiences() {
   return (
     <div className="experiences">
-      <h1>My Experience</h1>
+      <div className="title">My Experience</div>
       <div>
-        <h4>Front End Developer</h4>
-        <p>PNYX</p>
-        <p>Tempe,AZ</p>
-        <p>July 2023 - Present</p>
-        <p>
-          Spearhead front-end development for a music streaming web app
-          utilizing React, Flask, and PostgreSQL.
-          <br />
-          Enhance user engagement by creating a visually immersive 3D background
-          using CSS techniques.
-          <br />
-          Utilize React Router for efficient navigation between pages,
-          facilitating a smooth user journey.
-          <br />
-          Integrated ThirdwebWallet, aWeb3 authentication solution withWeb2 UX,
-          for secure and decentralized user login.
-          <br />
-          Implemented Docker to containerize project environment, leading to a
-          50% reduction in onboarding time and setup.
-        </p>
-        <h4>Software Developer Intern</h4>
-        <p>Szymon Zephan Capital Group</p>
-        <p>New York City, NY</p>
-        <p>January 2023 - May 2023</p>
-        <p>
-          Developed dashboard for Customer Relationship Management (CRM) web app
-          using React, including interactive charts and graphs created with
-          react-chartjs2.
-          <br />
-          Contributed to backend architecture of CRM dashboard, defining data
-          models, relationships & developing secure REST API.
-          <br />
-          Created bulk data insertion from Excel sheets with robust pessimistic
-          locking mechanism and tested using Apache JMeter, preventing data
-          conflicts and boosting user productivity by 80%.
-          <br />
-          Developed unit and integration test cases using Django test and
-          unittest, resulting reduction in API errors.
-        </p>
-        <h4>Web Developer</h4>
-        <p>Intrisol</p>
-        <p>Ahmedabad, India</p>
-        <p>June 2020 - June 2021</p>
-        <p>
-          Developed mobile-responsive product and cart page for e-commerce web
-          app from design mock ups using React. • Worked closely with backend
-          developers to integrate front-end code with server-side logic using
-          RESTful APIs. • Utilized CSS preprocessors like Sass to streamline
-          style sheet management and improve code maintainability. • Maintained
-          website content with GitHub and CI/CD, displaying an ability to adapt
-          to evolving project requirements.
-        </p>
+        {experiences.map((experience, index) => (
+          <div key={index} className="experience">
+            <p>
+              {experience.date} · {experience.title} · {experience.location}
+            </p>
+            <h2>{experience.company}</h2>
+            <ul>
+              {experience.description.map((descItem) => (
+                <li key={descItem}>{descItem}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
     </div>
   );
